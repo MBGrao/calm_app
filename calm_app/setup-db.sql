d881e0183fc0a88629bccceafbea71d33f0c9686
Mahesh <muhammadbingulzar@gmail.com>
2025-08-22 20:43:20 +0500

    Clean Flutter frontend repository - ready for Xcode development

--- calm_app/setup-db.sql
+++
@@ -1,8 +0,0 @@
--- MeditAi Database Setup
-CREATE DATABASE meditai_db;
-CREATE USER meditai_user WITH PASSWORD 'meditai_prod_password_2024';
-GRANT ALL PRIVILEGES ON DATABASE meditai_db TO meditai_user;
-\c meditai_db
-GRANT ALL ON SCHEMA public TO meditai_user;
-\q
-
